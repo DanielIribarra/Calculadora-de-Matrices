85b92d7b7dcf2ec4589ee96ad049f7ccaf56e5a4
Daniel <daniel.iribarra@gmail.com>
2025-09-22 01:45:33 -0300

    page

--- pr-alg2/src/App.svelte
+++ pr-alg2/src/App.svelte
@@ -1,34 +1,28 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import {Matrix} from './lib/matrix'
+	import svelteLogo from './assets/svelte.svg'
+	import viteLogo from '/vite.svg'
+	import {Matrix} from './lib/matrix'
+	import Layout from './Layout.svelte';
 
-  let M = new Matrix([3,3], [[1,2,5],[1,3,2],[2,4,9]]);
-  let A = new Matrix([3,3], [[1,2,5],[1,3,2],[2,4,9]]);
-  let B = new Matrix([3,4], [[1,1,1,1],[2,2,2,2],[1,1,1,1]]);
-  console.log(B);
-  /*
-  try {
-    console.log(M.getInverse());
-  } catch (error) {
-    console.log("M no es Invertible");
-  }
-  console.log(M.sum(A))
-  console.log(M.multiplication(A))
-  */
-  console.log(`Rango = ${B.getRank}`);
+	let M = new Matrix([3,3], [[1,2,5],[1,3,2],[2,4,9]]);
+	let A = new Matrix([3,3], [[1,2,5],[1,3,2],[2,4,9]]);
+	let B = new Matrix([3,4], [[1,1,1,1],[2,2,2,2],[1,1,1,1]]);
+	console.log(B);
+
+
 </script>
 
 <main>
-  <textarea id="input_inv1" class=""></textarea>
+	<Layout>
+		<label for="textarea1">A</label>
+		<textarea id="textarea1"></textarea>
+
+		<label for="textarea2">A<sup>-1</sup></label>
+		<textarea id="textarea2" readonly>(1 2 3)&NewLine;(1 2 3)&NewLine;(1 2 3)&NewLine;</textarea>
+
+		<button aria-label="Calcular">Calcular</button>
+	</Layout>
 </main>
 
 <style>
-  textarea {
-    background-color: #000;
-    border: 1px solid #000;
-    color: #00ff00;
-    padding: 8px;
-    font-family: courier new;
-  }
 </style>
---
+++ pr-alg2/src/Layout.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import svelteLogo from './assets/svelte.svg'
+	import viteLogo from '/vite.svg'
+</script>
+
+<main class="main-layout">
+	<div class="layout-header">
+		<img src="./src/assets/img/Analista-ComCientifica-blanco.png" alt="LogoLCC" class="logo">
+		<h2>Cálculadora de Matrices</h2>
+	</div>
+	<div class="layout-content">
+		<slot></slot>
+	</div>
+</main>
+
+<style>
+	.main-layout {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+	}
+	.layout-header {
+		flex: 0 1 100px;
+		padding: 0 1rem;
+		background-color: #000;
+  		border: 1px solid #000;
+		border-radius: 10px;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+	.layout-header > * {
+		margin: auto;
+	}
+	.layout-header .logo {
+		height: 75px;
+	}
+	.layout-content {
+		flex: 1 0 auto;
+		width: auto;
+		height: auto;
+		margin: 1rem;
+		display: flex;
+		flex-direction: column;
+		place-items: center;
+		gap: 1rem;
+	}
+</style>
